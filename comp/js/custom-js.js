6a54664dcec941b06fa7ebfacbf0e2204e9302a0
Jusar Culpable <jaxsar.culpable@gmail.com>
2026-02-25 16:02:34 +0800

    Projects Update

--- comp/js/custom-js.js
+++ comp/js/custom-js.js
@@ -4,3 +4,27 @@ const body = document.querySelector("body");
       btn.classList.toggle("active");
       body.classList.toggle("active");
 });
+
+
+fetch('/comp/json/project.json')
+.then(response => response.json())
+.then(projects => {
+    const projectList = document.querySelector('.project-box-list');
+
+    projects.slice(0, 3).forEach(project => {
+      const projectContainer = document.createElement('a');
+
+      projectContainer.href = `${project.link}`;
+      projectContainer.target = '_blank';
+      projectContainer.rel = 'nofollow noopener noreferrer';
+      projectContainer.innerHTML = `
+        <img src="${project.img}" >
+        <h3 class="my-8px">${project.name}</h3>
+        <p>${project.description}</p>
+      `;
+
+      projectList.appendChild(projectContainer);
+    });
+
+   
+})
